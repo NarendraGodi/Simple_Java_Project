1b1d4b112a5fbd90df58e455dd631487bf056263
Narendra Godi <narendragodi@gmail.com>
2022-10-18 05:08:38 +0000

    updated project1

--- src/main/java/jenkins_test/jenkins_test.java
+++ src/main/java/jenkins_test/jenkins_test.java
@@ -5,6 +5,7 @@ public class jenkins_test {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World from Jenkins Pipe Line");
+		System.out.println("Secondline from Jenkins Pipe Line");
 
 	}
 
